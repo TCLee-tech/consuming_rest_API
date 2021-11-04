3d7fa7fa99b0ea930720ee62ef48275babef5ff6
TCLee-tech <81542713+TCLee-tech@users.noreply.github.com>
2021-11-05 05:55:48 +0800

    Add files via upload
    API to consume streaming data, with filter rules
    Use @Autowired webClient .Builder and, webFlux container
    Data from Twitter Tweets
    I am coding with Visual Studio Code and JRE-17.

---
+++ consuming_streaming_data_with_rules_API_Twitter/src/test/java/com/linkedin/api/TwitterStreamWithRulesTest.java
@@ -0,0 +1,13 @@
+package com.linkedin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterStreamWithRulesTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
